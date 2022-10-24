72a8ee0013feb27c7b47bcd2f2ae2ca859b073bb
mukul <mukul.atreya@gmail.com>
2022-10-24 11:10:08 +0545

    connect wallet integration

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import heroBanner from '../assets/hero-banner.svg'
-import './Hero.css'
+import React, { useContext } from "react";
+import heroBanner from "../assets/hero-banner.svg";
+import "./Hero.css";
+import { DebountyContext } from "../context/DeBountyContext";
 
 function Hero() {
+  const { connectWallet, currentAccount } = useContext(DebountyContext);
+
   return (
     <section className="hero-sec">
-        <div className="left">
-            <h2>De-Bounty</h2>
-            <p>A decentralized solution to bug bounty</p>
-            <button className="btn">Connect Wallet</button>
-        </div>
-        <div className="right">
-            <img src={heroBanner} alt="" />
-        </div>
+      <div className="left">
+        <h2>De-Bounty</h2>
+        <p>A decentralized solution to bug bounty</p>
+
+        {currentAccount ? (
+          "Get Started"
+        ) : (
+          <button onClick={connectWallet} className="btn btn-wallet-connect">
+            Connect Wallet
+          </button>
+        )}
+      </div>
+      <div className="right">
+        <img src={heroBanner} alt="" />
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
---
+++ src/context/DeBountyContext.jsx
@@ -0,0 +1,98 @@
+//All smart contract connection & call will begin from here
+
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+import { contractABI, contractAddress } from "../utils/constants";
+
+export const DebountyContext = React.createContext();
+
+//provides metmask connection
+const { ethereum } = window;
+
+const createEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const deBountyContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+  return deBountyContract;
+};
+
+export const DebountyProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Mmask not detected");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setLoggedIn(true);
+      localStorage.setItem("loggedIn", JSON.stringify({ entry: true }));
+
+      console.log("Wallet connectes", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log("Error on wallet connection", error);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("loggedIn");
+    document.location.href = "/";
+    setLoggedIn(false);
+  };
+
+  const checkWalletConnection = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("No wallet detected, makesure yo have metamask wallet ");
+        return;
+      } else {
+        console.log("Wallet detected", ethereum);
+      }
+
+      const accounts = await ethereum.request({ methods: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Current active account", account);
+        setLoggedIn(true);
+        const logged = JSON.parse(localStorage.getItem("loggedIn")).entry;
+        logged && setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkWalletConnection();
+  }, []);
+
+  return (
+    <DebountyContext.Provider
+      value={{
+        connectWallet,
+        currentAccount,
+        logout,
+        checkWalletConnection,
+      }}
+    >
+      {children}
+    </DebountyContext.Provider>
+  );
+};
