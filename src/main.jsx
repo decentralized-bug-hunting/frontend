13f1f6a33b28ce64c53ce7d387a197b32b1e8db2
sauzzon <saujan.tiwari@gmail.com>
2022-11-11 11:10:55 +0545

    react toast added

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { DebountyProvider } from "./context/DeBountyContext";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DebountyProvider>
+      <ToastContainer
+        position="top-right"
+        hideProgressBar={true}
+        autoClose={5000}
+        closeButton={false}
+        closeOnClick={true}
+        newestOnTop={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <App />
     </DebountyProvider>
   </React.StrictMode>
